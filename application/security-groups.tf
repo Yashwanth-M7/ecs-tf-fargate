e516b53adc3a30108d6ac35e7b01903cad9d8bda
MYashwanth-wwt <165943938+MYashwanth-wwt@users.noreply.github.com>
2024-09-29 21:31:21 +0530

    doing modifications

--- application/security-groups.tf
+++ application/security-groups.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "ecs-alb-security-groups" {
   name = "${var.ecs-cluster-name}-alb-sg"
   description = "secuirty group for ecs alb"
-  vpc_id = data.terraform_remote_state.infrastructure.vpc_id
+  vpc_id = data.terraform_remote_state.infrastructure.outputs.vpc_id
 
   ingress {
     from_port  = 443
@@ -16,5 +16,4 @@ resource "aws_security_group" "ecs-alb-security-groups" {
     protocol = "-1"
     cidr_blocks = [ data.terraform_remote_state.infrastructure.outputs.vpc_cidr_block ]
   }
-
 }
