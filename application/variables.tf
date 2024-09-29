039e6ca00f39f7e761204c09d7b98d266b90af6c
MYashwanth-wwt <165943938+MYashwanth-wwt@users.noreply.github.com>
2024-09-29 22:08:48 +0530

    doing modifications

--- application/variables.tf
+++ application/variables.tf
@@ -3,8 +3,14 @@ variable "aws_region" {
 }
 
 
-variable "remote_state_bucket" {}
+variable "remote_state_bucket" {
+    description = "remote_state_bucket"
+}
 
-variable "remote_state_key" {}
+variable "remote_state_key" {
+    description = "remote_state_key"
+}
 
-variable "ecs-cluster-name" {}
+variable "ecs-cluster-name" {
+    description = "ecs-cluster-name"
+}
