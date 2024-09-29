f8048d4e2b71041bac6838d38abf7261c10762cf
MYashwanth-wwt <165943938+MYashwanth-wwt@users.noreply.github.com>
2024-09-29 22:40:32 +0530

    doing modifications

--- application/ecs.tf
+++ application/ecs.tf
@@ -66,22 +66,20 @@ resource "aws_iam_role" "ecs-cluster-role" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = ["ec2.amazonaws.com", "ecs.amazonaws.com", "application-autoscaling.amazonaws.com"]
-        }
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "${var.ecs-cluster-name}-role"
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+ {
+   "Effect": "Allow",
+   "Principal": {
+     "Service": ["ecs.amazonaws.com", "ec2.amazonaws.com", "application-autoscaling.amazonaws.com"]
+   },
+   "Action": "sts:AssumeRole"
   }
+  ]
+ }
+EOF
 }
 
 resource "aws_iam_role_policy" "ecs-cluster-policy" {
