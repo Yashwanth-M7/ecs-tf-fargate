63a7a062f228a52a0aace3c131538b6df34ee9a2
MYashwanth-wwt <165943938+MYashwanth-wwt@users.noreply.github.com>
2024-09-29 15:00:08 +0530

    tags added in all lines

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -52,7 +52,7 @@ resource "aws_subnet" "private-subnet-1" {
   vpc_id            = aws_vpc.production-vpc.id
   availability_zone ="${var.aws_region}a"
 
-  tags {
+  tags = {
     Name = "private-subnet-1"
   } 
 }
@@ -62,7 +62,7 @@ resource "aws_subnet" "private-subnet-2" {
   vpc_id            = aws_vpc.production-vpc.id
   availability_zone ="${var.aws_region}b"
 
-  tags {
+  tags = {
     Name = "private-subnet-2"
   } 
 }
@@ -72,7 +72,7 @@ resource "aws_subnet" "private-subnet-3" {
   vpc_id            = aws_vpc.production-vpc.id
   availability_zone ="${var.aws_region}c"
 
-  tags {
+  tags = {
     Name = "private-subnet-3"
   } 
 }
@@ -82,7 +82,7 @@ resource "aws_subnet" "private-subnet-3" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.production-vpc.id
 
-  tags {
+  tags = {
     Name = "public-route-table"
   }
 }
@@ -107,7 +107,7 @@ resource "aws_route_table_association" "public-subnet-3-association" {
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.production-vpc.id
 
-  tags {
+  tags = {
     Name = "private-route-table"
   }
 }
@@ -142,7 +142,7 @@ resource "aws_eip" "elastic-ip-for-nat-gw" {
   vpc = true
   associate_with_private_ip = "10.10.0.25"
 
-  tags {
+  tags = {
     Name = "Production-EIP"
   }
   depends_on = [ aws_internet_gateway.production-igw ]
@@ -154,7 +154,7 @@ resource "aws_nat_gateway" "production-nat-gw" {
   allocation_id = aws_eip.elastic-ip-for-nat-gw.id
   subnet_id = aws_subnet.public-subnet-1.id
 
-  tags {
+  tags = {
     Name = "Production-NAT-GW"
   }
   depends_on = [ aws_eip.elastic-ip-for-nat-gw ]
